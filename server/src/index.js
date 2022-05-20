9984c41689e2b64645741cee1e3b73bd9ec6c131
Evans Wanjau <evanswanjau@gmail.com>
2022-05-20 12:32:48 +0300

    Linked everything together - Imported resolvers and TrackAPI class - Added resovlers and dataSources to the initiated ApolloServer

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(9)],
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 const server = new ApolloServer({
-  typeDefs,
-  mocks,
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            trackAPI: new TrackAPI(),
+        };
+    },
 });
 
 server.listen().then(() => {
-  console.log(`
+    console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
     📭  Query at https://studio.apollographql.com/dev
